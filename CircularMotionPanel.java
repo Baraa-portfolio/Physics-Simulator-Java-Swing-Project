2320f217dc36e8793d341977719d8b1af42d23f6
Baraa-portfolio <979902@pdsb.net>
2026-06-03 23:31:09 -0400

    Update CircularMotionPanel.java

--- CircularMotionPanel.java
+++ CircularMotionPanel.java
@@ -49,3 +49,8 @@ public class CircularMotionPanel extends JPanel {
             rField.setText("1.0");
             vField.setText("0.0");
         });
+        bottom.add(reset);
+
+        add(bottom, BorderLayout.SOUTH);
+    }
+}
